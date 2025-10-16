a3c507ba9e3ec12262df8b2a3c35d36d2947d63a
Maurício Hendrikx <mauricio.servatius@gmail.com>
2025-10-16 08:54:16 -0300

    main

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-docker compose -f /home/ubuntu/apps/docker/docker-compose.yml down karatuben
-docker rmi mservatius/karatuben:arm
-docker build -t karatuben:arm .
-docker tag karatuben:arm mservatius/karatuben:arm
-docker push mservatius/karatuben:arm
-docker rmi karatuben:arm
-docker rmi mservatius/karatuben:arm
-docker compose -f /home/ubuntu/apps/docker/docker-compose.yml up karatuben -d
+docker compose -f /home/msh/docker/compose/docker-compose.yml down karatuben
+docker rmi mservatius/karatuben:latest
+docker build -t karatuben:latest .
+docker tag karatuben:latest mservatius/karatuben:latest
+docker push mservatius/karatuben:latest
+docker rmi karatuben:latest
+docker rmi mservatius/karatuben:latest
+docker compose -f /home/msh/docker/compose/docker-compose.yml up karatuben -d
